8371546bbe3971ed3c6b1905e51094e776c96dcc
royce-reed <rjreed1@gmail.com>
2022-06-02 12:21:32 -0500

    built skeleton for audiobooks component

--- client/src/components/AudioBooks/AudioBook.jsx
+++ client/src/components/AudioBooks/AudioBook.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import { TextField, Typography } from '@material-ui/core';
+import axios from 'axios';
 const AudioBook = () => {
+
+  const getAudioBooks = () => {
+    // const options = {
+    //   method: 'GET',
+    //   url: '/audioBooks',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json;charset=UTF-8'
+    //   },
+    // };
+    axios.get('https://librivox.org/api/feed/audiobooks?format=json').then((data) => {
+      console.log(data, 'DATA');
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+  
   return (
     <div className='audio-book'>
-      <h3>audiobooks</h3>
+      <Typography variant='h5'>Audio Books</Typography>
+      <Button onClick={getAudioBooks} variant="contained" id="outlined-basic" color="primary">Get Audio Books</Button>
     </div>
   );
 };
