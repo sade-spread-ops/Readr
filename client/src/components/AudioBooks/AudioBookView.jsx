dd5df08dd018b2e96b6fb52d350b28e4c5f0e9d1
royce-reed <rjreed1@gmail.com>
2022-06-04 13:40:34 -0500

    added conditional rendering for audiobooks array

--- client/src/components/AudioBooks/AudioBookView.jsx
+++ client/src/components/AudioBooks/AudioBookView.jsx
@@ -3,29 +3,30 @@ import React, { useState } from 'react';
 
 
 const AudioBookView = (props) => {
+  // console.log(props);
+  const { audiobooks } = props;
   return (
-    <div className="audio-book-view">
-      {
-        props.audiobooks.map((book) => ( 
-          <div className="audio-book-view-item" key={book.id}>
-            <b>{book.title}</b>
-            {book.authors.map((author) => (
-              <div className="audio-book-view-item-author" key={author.id}>
-                <i>{`${author.first_name} ${author.last_name}`}</i>
-              </div>
-            ))}
-            <div className="audio-book-view-item-zip-download">
-              <a href={book.url_zip_file}>Download a zip file</a>
+    <div className="audiobook-view">
+      {audiobooks && audiobooks.map((audiobook) => (
+        <div className="audiobook-view-item" key={audiobook.id}>
+          <b>{audiobook.title}</b>
+          {audiobook.authors.map((author) => (
+            <div className="audiobook-view-item-author" key={author.id}>
+              <i>{`${author.first_name} ${author.last_name}`}</i>
             </div>
-            <div className="audio-book-view-item-librivox-listen">
-              <a href={book.url_librivox} target="_blank">Listen Online at Librivox</a>
-            </div>
-            <div>
-            **************************************************
-            </div>
+          ))}
+          <div className="audiobook-view-item-zip-download">
+            <a href={audiobook.url_zip_file}>Download a zip file</a>
           </div>
-        ))
-      }
+          <div className="audiobook-view-item-librivox-listen">
+            <a href={audiobook.url_librivox} target="_blank">Listen Online at Librivox</a>
+          </div>
+          <div>
+          **************************************************
+          </div>
+        </div>
+      ))}
+      
     </div>
   );
 };
